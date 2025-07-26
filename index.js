ddd796df6bd199f01b354db4d4296c1a3b88f7ed
Fatai <fsule@icloud.com>
2025-07-25 20:49:06 -0400

    update server js and index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('#contact form');
 
-  form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', async function(event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Get form values
@@ -13,7 +13,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const body = `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`;
 
     // Construct the mailto link
-    const mailtoLink = `mailto:info@kfsquare.com?subject=Contact Form Submission&body=${encodeURIComponent(body)}`;
+    // const mailtoLink = `mailto:info@kfsquare.com?subject=Contact Form Submission&body=${encodeURIComponent(body)}`;
 
     // Open the email client
-    window.location.href = mailtoLink;
+    // window.location.href = mailtoLink;
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // sgMail.setDataResidency('eu');
+    // uncomment the above line if you are sending mail using a regional EU subuser
+
+    const msg = {
+      to: 'info@kfsquare.com', // Change to your recipient
+      from: email, // Change to your verified sender
+      subject: 'Info from kfsquare.com site',
+      text: message,
+      html: message,
+    };
+
+    try {
+      await sgMail.send(msg);
+      console.log('Email sent');
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
